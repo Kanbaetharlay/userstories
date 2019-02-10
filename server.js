a45424bda39dbf0e6c24a08dd1ca71b7e34742d0
AungPyaePhyo <33777420+Kanbaetharlay@users.noreply.github.com>
2019-02-10 19:20:23 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const port = 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 //mysql connect
+//mysql://b707a0f5755ac2:0fee9df8@us-cdbr-iron-east-03.cleardb.net/heroku_a00ac536a4451ad?reconnect=true
 const mysql = require('mysql');
 var db = mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "",
-    database : "userstories"
+    host : "us-cdbr-iron-east-03.cleardb.net",
+    user : "b707a0f5755ac2",
+    password : "0fee9df8",
+    database : "heroku_a00ac536a4451ad"
 })
 db.connect((err) => {
     if(err) {
